4a9146e74d6919fbbaac392b80ae7009517a84a0
Leigh Johnson <leigh@bitsy.ai>
2025-11-26 09:53:44 -0800

    fixup: rename script

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -1,146 +1,188 @@
-import itertools
-from uuid import uuid4
-import json
-
-from dataclasses import replace
-import mwpf.ref_circuit
-
-import hashlib
-import sinter
-import stim
-from mwpf import SinterMWPFDecoder
-import matplotlib.pyplot as plt
+import click
+from dataclasses import dataclass
+from typing import Optional
 import os
-
-from tqdm.notebook import tqdm
-
-CONVERT_TO_ERASURE_NOISE = ("DEPOLARIZE1", "DEPOLARIZE2")
-noise = [0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3]
-noise = [
-    0.001,
-    0.004,
-    0.005,
-    0.006,
-    0.0065,
-    0.007,
-    0.0075,
-    0.008,
-    0.009,
-    0.01,
-    0.011,
-]
-code_distance = [3, 5, 7, 9, 11, 13, 15]
-MAX_SHOTS = 10_000
-MAX_ERRORS = 1_000
+from util.sinter_task import (
+    CollectTasksConfig,
+    collect_stats,
+    MWPFSolverType,
+    DecoderLib,
+)
 
 
-def erasure_conversion(
-    task_config: dict, conversion_factor: int = 1, add_detectors: bool = True
-) -> dict:
-    """Converts random pauli noise instructions (e.g. DEPOLARIZE1, DEPOLARIZE2) to HERALDED_ERASE instructions
-
-    See notes in https://github.com/yuewuo/mwpf/blob/main/src/python/mwpf/ref_circuit.py
-    for explanation of why reference circuits are needed to keep track of measurement indices w/ heralded errors
-    """
-    circuit = task_config["circuit"]
-    json_metadata = task_config["json_metadata"]
-    ref_circuit = mwpf.ref_circuit.RefCircuit.of(circuit)
-
-    instructions: list[mwpf.ref_circuit.RefInstruction] = []
-    for instruction in ref_circuit:
-        if instruction.name in CONVERT_TO_ERASURE_NOISE:
-            p = instruction.gate_args[0]
-            instructions.append(replace(instruction, gate_args=[p * conversion_factor]))
-            new_instruction = mwpf.ref_circuit.RefInstruction.new_heralded_erase(
-                instruction.targets, min(1, 4 / 3 * p * conversion_factor)
-            )
-            instructions.append(new_instruction)
-            if add_detectors is True:
-                for rec in new_instruction.recs:
-                    instructions.append(
-                        mwpf.ref_circuit.RefInstruction.new_detector((rec,))
-                    )
-        else:
-            instructions.append(instruction)
-    converted_metadata = json_metadata.copy()
-    converted_metadata["p_erase"] = converted_metadata["p"]
-    converted_circuit = mwpf.ref_circuit.RefCircuit.of(instructions).circuit()
-
-    new_task_config = dict(
-        circuit=converted_circuit,
-        json_metadata=converted_metadata,
-    )
-    return new_task_config
+@dataclass
+class CommonOpts:
+    circuit: str
+    max_errors: int
+    max_shots: int
+    noise: tuple[float, ...]
+    code_distance: tuple[int, ...]
+    save_resume_filepath: str
+    quiet: bool = False
+    num_workers: int = os.cpu_count() - 2
 
 
-def generate_task_config(d, p):
-    circuit = stim.Circuit.generated(
-        "surface_code:rotated_memory_x",
-        rounds=3 * d,  # since we have perfect measurements, run 1 round
-        distance=d,
-        before_round_data_depolarization=p,
-    )
-    cluster_node_limit = 50
-    # max_shots = 50_000
-    # max_errors = 2_000
-    # max_shots = 10000
-    # max_errors = 100
-    # elif d >= 7:
-    #     cluster_node_limit = 100
-    #     max_shots = 10_000
-    #     max_errors = 1_000
-    json_metadata = {
-        "d": d,
-        "p": p,
-        "label": "rotated_memory_x",
-        "r": d * 3,
-        "decoder": "mwpf_erasure",
-        "cluster_node_limit": cluster_node_limit,
-    }
-    # collection_options = {"max_shots": max_shots, "max_errors": max_errors}
-    run_id = hashlib.sha256(json.dumps(json_metadata))
-    json_metadata["run_id"] = run_id
-    cfg = dict(
+def common_options(f):
+    f = click.option(
+        "--num-workers",
+        "num_workers",
+        type=int,
+        default=os.cpu_count() - 2,
+        show_default=True,
+        help="Number of sinter (multiprocessing) workers. NOTE: sinter uses spawn (not fork) to create subprocesses, so each additional worker process adds cold start time sinter.collect() calls. Prefer a high number of worker processes with few (ideally one) call to sinter.collect().",
+    )(f)
+    f = click.option(
+        "--code-distance",
+        "code_distance",
+        type=int,
+        multiple=True,
+        default=(3, 5, 7, 9, 11, 13, 15),
+        show_default=True,
+        help="Repeatable. Example: --code-distance 3 --code-distance 5",
+    )(f)
+    f = click.option(
+        "--noise",
+        type=float,
+        multiple=True,
+        default=(
+            0.001,
+            0.004,
+            0.005,
+            0.006,
+            0.0065,
+            0.007,
+            0.0075,
+            0.008,
+            0.009,
+            0.01,
+            0.011,
+        ),
+        show_default=True,
+        help="Repeatable. Example: --noise 0.01 --noise 0.02",
+    )(f)
+    f = click.option(
+        "--max-shots",
+        default=100_000,
+        show_default=True,
+        help="Max number of shots (trials) in Monte Carlo simulation.",
+    )(f)
+    f = click.option(
+        "--max-errors",
+        default=5_000,
+        show_default=True,
+        help="Max number of errors in Monte Carlo simulator shots.",
+    )(f)
+    f = click.option(
+        "--circuit",
+        type=click.Choice(["surface_code:rotated_memory_x"]),
+        default="surface_code:rotated_memory_x",
+        show_default=True,
+    )(f)
+    f = click.option("--quiet", is_flag=True, default=False, help="Silence progress")(f)
+
+    f = click.option(
+        "--save-resume-filepath",
+        default="auto",
+        help="Specify a save/resume filepath to pass to sinter.collect. If 'auto', a path will be generated from dataset parameters",
+    )(f)
+    # TODO
+    # f = click.option(
+    #     "--fanout",
+    #     is_flag=True,
+    #     default=False,
+    #     help="If true, fanout sinter.collect() and resulting dataset files (typically per unique pair of (d, p) parameters). Fanout is needed for mwpf w/ erasure enabled, so the save/resume checkpoints are more granular. For other decoders (pymatching), fanout is generally not needed and is slower, so specify only if you.  want to generate granular dataset files)",
+    # )(f)
+    return f
+
+
+@click.group()
+@common_options
+@click.pass_context
+def cli(
+    ctx,
+    num_workers,
+    circuit,
+    max_errors,
+    max_shots,
+    noise,
+    code_distance,
+    save_resume_filepath,
+    quiet,
+):
+    ctx.ensure_object(dict)
+    ctx.obj["common"] = CommonOpts(
         circuit=circuit,
-        json_metadata=json_metadata,
-        # collection_options=collection_options,
+        max_errors=max_errors,
+        max_shots=max_shots,
+        noise=noise,
+        code_distance=code_distance,
+        quiet=quiet,
+        save_resume_filepath=save_resume_filepath,
+        num_workers=num_workers,
     )
-    erasure_cfg = erasure_conversion(cfg)
-    task = sinter.Task(**erasure_cfg, decoder=f"mwpf__{run_id}")
-    return task
 
 
-def build_custom_decoders(task_configs):
-    return {
-        f"mwpf__{task.json_metadata['run_id']}": SinterMWPFDecoder(
-            decoder_type="SolverSerialJointSingleHair",
-            cluster_node_limit=task.json_metadata["cluster_node_limit"],
-            with_progress=True,
-            timeout=10,
-        ).with_circuit(task.circuit)
-        for task in task_configs
-    }
+@click.command()
+@click.pass_context
+def pymatching(ctx):
+    common: CommonOpts = ctx.obj["common"]
+    cfg = CollectTasksConfig(
+        circuit=common.circuit,
+        save_resume_filepath=common.save_resume_filepath,
+        decoder=DecoderLib.PYMATCHING,
+        max_shots=common.max_shots,
+        max_errors=common.max_errors,
+        num_workers=common.num_workers,
+        erasure_conversion_factor=0.0,
+        noise=common.noise,
+        code_distance=common.code_distance,
+    )
+    collect_stats(cfg)
+    # TODO: write CollectTasksConfig (serialized to JSON) to {save_resume_filepath}vars.json"
 
 
-def collect_erasure_stats(code_distance, noise):
-    pbar = tqdm(code_distance)
-    results = []
-    for d in pbar:
-        pbar.set_description(f"Collecting d={d}")
-        tasks = list(generate_task_config(d, p) for p in noise)
-        custom_decoders = build_custom_decoders(tasks)
+@click.option(
+    "--cluster_node_limit",
+    default=50,
+    show_default=True,
+    help="Hyperblossom decomposes a hypergraph problem into smaller, localized clusters. c=0 implies the UnionFind strategy, while any c >=1 will use the SerialJointSingleHair strategy. See description of c= parameter in https://arxiv.org/pdf/2508.04969",
+)
+@click.option(
+    "--erasure-conversion-factor",
+    default=0.0,
+    show_default=True,
+    help="Convert % of Pauli errors to erasures. 0 implies no conversion (default), 1 implies 100% conversion.",
+)
+@click.option(
+    "--solver",
+    type=click.Choice(MWPFSolverType),
+    default=MWPFSolverType.SolverSerialJointSingleHair,
+    show_default=True,
+    help="See: https://github.com/yuewuo/mwpf/blob/main/src/mwpf_solver.rs",
+)
+@click.command()
+@click.pass_context
+def mwpf(ctx, cluster_node_limit, erasure_conversion_factor, solver):
+    common: CommonOpts = ctx.obj["common"]
+    cfg = CollectTasksConfig(
+        circuit=common.circuit,
+        save_resume_filepath=common.save_resume_filepath,
+        decoder=DecoderLib.MWPF,
+        decoder_type=solver,
+        max_shots=common.max_shots,
+        max_errors=common.max_errors,
+        num_workers=common.num_workers,
+        noise=common.noise,
+        code_distance=common.code_distance,
+        cluster_node_limit=cluster_node_limit,
+        erasure_conversion_factor=erasure_conversion_factor,
+    )
+    collect_stats(cfg)
 
-        erasure_collected_stats = sinter.collect(
-            num_workers=os.cpu_count() - 4,
-            tasks=tasks,
-            custom_decoders=custom_decoders,
-            print_progress=True,
-            max_shots=MAX_SHOTS,
-            max_errors=MAX_ERRORS,
-            save_resume_filepath=f"../datasets/rotated_memory_x_mwpf_SolverSerialJointSingleHair_erasure/2025_11_24_pthresh__d={d}.csv",
-        )
-        results.append(erasure_collected_stats)
-    return results
+
+cli.add_command(pymatching)
+cli.add_command(mwpf)
 
 
-erasure_collected_stats = collect_erasure_stats(code_distance, noise)
+if __name__ == "__main__":
+    cli()
