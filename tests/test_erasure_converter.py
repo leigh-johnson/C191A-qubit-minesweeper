8f7e1b5c95a2de9201907c127a055504c827c4b7
Leigh Johnson Zaman <leigh@bitsy.ai>
2025-11-26 09:56:44 -0800

    CLI: generate surface code threshold datasets (#19)
    * wip: trying to validate our surface code thresholds
    
    * freeze: pymatching threshold
    
    * simulate up to d=15
    
    * wip: converting notebook to script
    
    * ignore /tmp
    
    * test: convert_circuit_errors_to_erasures with conversion_factor=1
    
    * pin deps: click, ipython kernel plugins
    
    * wip: make_dataset cli cmd
    
    * test: save/resume filepath generator
    
    * cli: pymatching threshold dataset
    
    * fixup: failing tests
    
    * wip: mwpf dataset generator cmd
    
    * fixup: erasure converter err
    
    * fixup: extra .csv ext
    
    * dataset: mwpf w/ erasure_conversion_factor=0
    
    * gitignore datasets/
    
    * cleanup checked in datasets
    
    * fixup: rename script

---
+++ tests/test_erasure_converter.py
@@ -0,0 +1,33 @@
+import stim
+from util.erasure_converter import (
+    convert_circuit_errors_to_erasures,
+    filter_convert_to_erasure_instructions,
+)
+from util.sinter_task import TaskMetadata, DecoderLib, TaskConfig
+
+
+def test_surface_code_layout_conversion_factor_equals_1():
+    p = 0.01
+    d = 3
+    r = d * 3
+    conversion_factor = 1
+    circuit = stim.Circuit.generated(
+        "surface_code:rotated_memory_x",
+        rounds=r,
+        distance=d,
+        before_round_data_depolarization=p,
+    )
+    metadata = TaskMetadata(
+        p=p, d=d, r=r, circuit="surface_code:rotated_memory_x", decoder=DecoderLib.MWPF
+    )
+    task = TaskConfig(circuit=circuit, json_metadata=metadata)
+    result = convert_circuit_errors_to_erasures(
+        task, conversion_factor=conversion_factor
+    )
+    modified_circuit = result.circuit
+    erased_errors = filter_convert_to_erasure_instructions(modified_circuit)
+
+    # if the conversion factor is 1, we expect all depolarizing noise to have p=0 in the modified circuit
+    for err in erased_errors:
+        gate_args = err.gate_args_copy()
+        assert gate_args[0] == 0
