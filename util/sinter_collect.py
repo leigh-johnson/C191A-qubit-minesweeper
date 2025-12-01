5ebbdf4d7a8a3192681d73393b26b24b980a37b3
Leigh Johnson Zaman <leigh@bitsy.ai>
2025-11-30 18:57:19 -0800

    Custom HERALDED_ERASE-aware minimum-weight perfect matching decoder (#43)
    * wip: building a custom pymatching edge-weight decoder
    
    * fixup: noise sweep
    
    * cli: implement custom pymatching decoder for HERALDED_ERASE instructions
    
    * re-pin mwpf
    
    * fixup: failing tests
    
    * fix typo
    
    * check in: dataset + notebook run

--- util/sinter_collect.py
+++ util/sinter_collect.py
@@ -10,10 +10,12 @@ from datetime import date
 
 import sinter
 import stim
-from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 
-from util.erasure_converter import convert_circuit_errors_to_erasures
+from util.erasure_converter import (
+    convert_circuit_errors_to_erasures_v1,
+    convert_task_config_to_erasures,
+)
 
 from util.sinter_task import (
     DecoderLib,
@@ -72,7 +74,7 @@ class CollectTasksConfig:
             packed_args,
             max_workers=self.num_workers,
             disable=self.quiet,
-            desc="Preparting sinter.Task array",
+            desc="Preparing sinter.Task array",
             total=len(parameters),
         )
         for result in results:
@@ -124,13 +126,15 @@ def build_task_config(params: Tuple[int, int, CollectTasksConfig]):
         verbose=cfg.verbose,
     )
     if cfg.erasure_conversion_factor > 0:
-        if cfg.decoder is not DecoderLib.MWPF:
-            raise NotImplemented(
-                f"MWPF decoder is required for nonzero erasure_conversion_factor"
+        if cfg.erasure_conversion_factor > 0:
+            if cfg.decoder is DecoderLib.MWPF:
+                task_config = convert_circuit_errors_to_erasures_v1(
+                    task_config, cfg.erasure_conversion_factor
+                )
+        elif cfg.decoder is DecoderLib.PYMATCHING_CORRELATED:
+            task_config = convert_task_config_to_erasures(
+                task_config, cfg.erasure_conversion_factor
             )
-        task_config = convert_circuit_errors_to_erasures(
-            task_config, cfg.erasure_conversion_factor
-        )
     task = task_config.to_task()
     return (task_config, task)
 
@@ -140,9 +144,9 @@ def generate_save_resume_filepath(cfg: CollectTasksConfig):
     basedir = os.path.join(
         "datasets", f"circuit={cfg.circuit}", f"decoder={cfg.decoder}"
     )
-    if cfg.decoder == DecoderLib.PYMATCHING:
+    if cfg.decoder is DecoderLib.PYMATCHING:
         return Path(os.path.join(basedir, f"{iso_today}.csv"))
-    elif cfg.decoder == DecoderLib.MWPF:
+    elif cfg.decoder is DecoderLib.MWPF:
         return Path(
             os.path.join(
                 basedir,
@@ -151,15 +155,18 @@ def generate_save_resume_filepath(cfg: CollectTasksConfig):
                 f"{iso_today}.csv",
             )
         )
+    elif cfg.decoder is DecoderLib.PYMATCHING_CORRELATED:
+        return Path(
+            os.path.join(
+                basedir,
+                f"erasure={cfg.erasure_conversion_factor}",
+                f"{iso_today}.csv",
+            )
+        )
     else:
         raise NotImplemented(
             f"generate_save_resume_filepath not implemented for decoder {cfg.decoder}"
         )
-
-
-def generate_run_id(cfg: TaskConfig):
-    run_id = hashlib.sha256(json.dumps(cfg.json_metadata).encode("utf-8")).hexdigest()
-    return run_id
 
 
 def collect_stats(cfg: CollectTasksConfig) -> List[sinter.TaskStats]:
